94a62ec0048d6bb7cccc66330d29737510fa8e66
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-22 11:19:03 +0530

    the file has been updated

---
+++ intro.js
@@ -0,0 +1,14 @@
+console.log("hello javascript");
+let accountemail = "web.bocket@gmail.com";
+var password = "234555";
+accoutncity = "bbsr";
+let accountstate;
+// console.log(accountemail);
+// console.log(password);
+// console.log(accoutncity);
+// console.log(accountstate);
+
+accountemail = "rajalaxmikhatai2002@gmail.com";
+console.log(accountemail);
+
+console.table([accountemail,accountstate,password,accoutncity]);
