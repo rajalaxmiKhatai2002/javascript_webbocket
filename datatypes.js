770be2304340ba81ad79c381bd00acd58eb0b7ce
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-24 10:12:04 +0530

    here the file has been updated

--- datatypes.js
+++ datatypes.js
@@ -1,12 +1,21 @@
-"use strict"; //treate all js code as newwer version
+// "use strict"; //treate all js code as newwer version
 
-console.log(3 + 3);//code readability should be high
+// console.log(3 + 3);//code readability should be high
 
-let name = "ritesh"
-let age = 18
-let isLoggedIn = false
-let state;
-let city = null
+// let name = "ritesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+// let city = null
 
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof name);
+// console.log(typeof age);
+//we use typeof operator to show that which type of datatype we use
+
+
+// conversion operation:-----------------------
+let score = "ritesh";
+console.log(typeof score);//string
+console.log(typeof(score));//string
