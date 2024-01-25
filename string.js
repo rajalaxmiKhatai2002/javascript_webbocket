1638b934a3cd9afa07fcc060859529d1a54503dc
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-25 09:44:38 +0530

    one string.js file has been updated

--- string.js
+++ string.js
@@ -1,4 +1,12 @@
-const name = "rajalaxmi"
-const repoCount = 50
-console.log(name + repoCount + "value");
-console.log('hello my name is ${name} and my repo count is ${repoCount}');
+// const name = "rajalaxmi"
+// const repoCount = 50
+// console.log(name + repoCount + "value");
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// const gameName = new String('zymps-zp-com')
+// console.log(gameName[8]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('m'));
