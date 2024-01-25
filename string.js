bf377a9f9998c001e165abcdde7be385652a5f66
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-25 10:14:57 +0530

    the file has been updated

--- string.js
+++ string.js
@@ -4,9 +4,39 @@
 // console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 
-// const gameName = new String('zymps-zp-com')
+const gameName = new String('zymps-zp-com')
 // console.log(gameName[8]);
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('m'));
+
+
+// const newString = gameName.slice(-8,4);
+
+// const newStringOne = " zympsy "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://zympsy.com/zympsy%20Panda"
+// console.log(url.replace('%20' , '-'));
+// console.log(url.includes('Panda'));
+// console.log(gameName);
+// console.log(url.split('/'));
+
+
+// const score = 400
+// // console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const othernumber = 123.8966;
+console.log(othernumber.toPrecision(4));
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+console.log(hundred.toLocaleString('en-US'));
