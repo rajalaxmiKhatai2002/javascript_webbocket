c5af2d7df05929e98d9e2c7b85d49a4ab2089ebe
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-25 11:11:36 +0530

    the file has been updated

--- string.js
+++ string.js
@@ -68,15 +68,32 @@ const gameName = new String('zymps-zp-com')
 // console.log(2 != 1);
 
 //string comparision
-console.log("2">1);
-console.log("02">1);
+// console.log("2">1);
+// console.log("02">1);
 
-//null comparision
-console.log(null>0);
-console.log(null == 0);
-console.log(null >= 0);
+// //null comparision
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-//undefined comparision
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// //undefined comparision
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//**************date******************** */
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+console.log(typeof mydate);
+
+let mycreatedate = new Date("01-14-2023")
+console.log(mycreatedate.toLocaleString()); //method in js used to convert date object
+
+let myitemstamp = Date.now()
+
+console.log(myitemstamp);
+console.log(mycreatedate.getTime());
+console.log(Math.floor(Data.now()/1000));
+
