dca88e03a1495e45f14fd2d82a580c502f0ef85c
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-27 11:31:31 +0530

    the file has been updated

--- object.js
+++ object.js
@@ -43,3 +43,90 @@ console.log(JsUser);
 // ``= ES6 literals
 // string interpolation
 // template literal
+
+
+
+
+//OBJECT2:-----------------
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "human"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "mailto:some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "zympsy",
+            lastname: "panda"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "mailto:zympsy@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "mailto:zympsy@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "mailto:zympsy@gmail.com"
+//     },
+// ]
+
+// users[1].email
+// // console.log(tinderUser);
+
+// // console.log(Object.keys(tinderUser));
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// const course = {
+//     coursename: "javascript",
+//     price: "999k",
+//     courseInstructor: "zympsy"
+// }
+
+// // course.courseInstructor
+
+// const {courseInstructor: instructor} = course //object de-structure
+
+// // console.log(courseInstructor);
+// console.log(instructor);
+
+// {
+//     "name": "zympsy",
+//     "coursename": "javascript",
+//     "price": "free"
+// }how  to structure an object within an array this is in the given bellow structure
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
