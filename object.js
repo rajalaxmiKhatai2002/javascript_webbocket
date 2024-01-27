322a9a407cfe759809bb0bb2ced9e6da483c54a4
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-27 10:47:19 +0530

    the file has been updated

--- object.js
+++ object.js
@@ -22,22 +22,22 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "zympsy@chatgpt.com"
-console.log(JsUser);
-// Object.freeze(JsUser)
-JsUser.email = "zympsy@microsoft.com"
-console.log(JsUser);
+// JsUser.email = "zympsy@chatgpt.com"
+// console.log(JsUser);
+// // Object.freeze(JsUser)
+// JsUser.email = "zympsy@microsoft.com"
+// console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-console.log(JsUser.greeting());
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// console.log(JsUser.greeting());
 
 
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`) ;
-}
-console.log(JsUser.greetingTwo());
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`) ;
+// }
+// console.log(JsUser.greetingTwo());
 
 
 // ``= ES6 literals
