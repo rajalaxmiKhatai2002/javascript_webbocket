f778257955122f8e536972e194514bf96bc894c1
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-30 11:33:20 +0530

    the file has been upadted

--- one.js
+++ one.js
@@ -356,25 +356,25 @@ const newNums = myNumers.map( (num) => { return num + 10})
 // console.log(myTotal);
 
 
-// const shoppingCart = [
-//     {
-//         itemName: "js course",
-//         price: 2999
-//     },
-//     {
-//         itemName: "py course",
-//         price: 999
-//     },
-//     {
-//         itemName: "mobile dev course",
-//         price: 5999
-//     },
-//     {
-//         itemName: "data science course",
-//         price: 12999
-//     },
-// ]
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
 
-// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
 // console.log(priceToPay);
