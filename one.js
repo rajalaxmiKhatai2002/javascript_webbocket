098c71466310f23a5188b9eddb6140cc7e3df565
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-30 09:46:04 +0530

    the file has been upadted

--- one.js
+++ one.js
@@ -131,5 +131,58 @@ let val1;
 
 // condition ? true : false
 
-const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80") 
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80") 
+
+
+
+
+
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 8) {
+        // console.log("8 is best number");
+    }
+    // console.log(element);
+    
+}
+
+// console.log(element);
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value: ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//     // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     // console.log(i + '*' + j + ' = ' + i*j );
+//    }
+    
+// }
+// let myArray = ["flash", "batman", "superman"]
+// // console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element); 
+// }
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 9) {
+        console.log(`Detected 9`);
+        break
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
