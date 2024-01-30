2f971c7165d1ba47b7d20233ccf9c27152175c7a
rajalxmi2002 <22mca.rajalaxmikhatai@gietbbsr.edu.in>
2024-01-30 10:02:20 +0530

    the file has been upadted

--- one.js
+++ one.js
@@ -199,16 +199,58 @@ for (let index = 1; index <= 20; index++) {
 //     index = index + 2
 // }
 
-let myArray = ["flash", "batman", "superman"];
+// let myArray = ["flash", "batman", "superman"];
 
-let arr = 0;
-while (arr < myArray.length) {
-  console.log(`Value is ${myArray[arr]}`);
-  arr = arr + 1;
-}
+// let arr = 0;
+// while (arr < myArray.length) {
+//   console.log(`Value is ${myArray[arr]}`);
+//   arr = arr + 1;
+// }
 
 // let score = 11;
 // do {
 //   console.log(`Score is ${score}`);
 //   score++;
 // } while (score <= 10);
+
+
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     //console.log(num);
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+// }
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+console.log(map);
+
+// for (const [key, value] of map) {
+//     // console.log(key, ':-', value);
+// }
+
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
